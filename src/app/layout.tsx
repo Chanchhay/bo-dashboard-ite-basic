8621a23920b20e175a577c0edf3ddee29b340c51
Yuma-backend-dev <yumarayya25@gmail.com>
2026-08-21 23:54:31 +0700

    change ui of category of inventory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import { Google_Sans } from "next/font/google";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/dark-mode/theme-provider";
 import StoreProvider from "./StoreProvider";
 import { NetworkStatusBanner } from "@/components/common/NetworkStatusBanner";
-
-const googleSans = Google_Sans({
-  variable: "--font-google-sans",
-  subsets: ["latin"],
-  display: "swap",
-  adjustFontFallback: false,
-});
 
 export const metadata: Metadata = {
   title: "FluxiBiz - Business Owner Dashboard",
@@ -24,11 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${googleSans.variable} h-full antialiased`}
-    >
+    <html lang="en" suppressHydrationWarning className="h-full antialiased">
       <body className="min-h-full flex flex-col">
         <NetworkStatusBanner />
         <ThemeProvider
